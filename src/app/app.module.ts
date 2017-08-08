8e45e4695e496d4f0557579ea75386079a80f213
dimitar.rabadziski <dimitar.rabadziski@my.sorsix.com>
2017-08-08 15:01:19 +0200

    test2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { EditEventComponent } from './edit-event/edit-event.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import {NgxPaginationModule} from 'ngx-pagination';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { Test2Component } from './test2/test2.component';
 
 
 
@@ -40,7 +41,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
     MyEventsComponent,
     EditEventComponent,
     UserProfileComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
